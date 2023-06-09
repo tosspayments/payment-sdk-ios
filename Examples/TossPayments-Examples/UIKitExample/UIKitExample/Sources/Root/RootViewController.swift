7a739f9c9c3e6e0c5badfdaa52c5455697d693e7
김진규 <mqz@toss.im>
2023-06-09 11:59:13 +0900

    feature: 예제를 위한 세팅 추가

--- Examples/TossPayments-Examples/UIKitExample/UIKitExample/Sources/Root/RootViewController.swift
+++ Examples/TossPayments-Examples/UIKitExample/UIKitExample/Sources/Root/RootViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 final class RootViewController: ViewController {
-    private lazy var 테스트키화면: TextField = {
+    private lazy var 클라이언트키화면: TextField = {
         let textField = TextField()
         textField.title = "Client Key"
         textField.text = Environment.clientKey
+        textField.textField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    private lazy var 커스터머키화면: TextField = {
+        let textField = TextField()
+        textField.title = "Customer Key"
+        textField.text = Environment.customerKey
+        textField.textField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    private lazy var 브랜드키리다이렉트화면: TextField = {
+        let textField = TextField()
+        textField.title = "Redirect URL"
+        textField.text = Environment.brandPayRedirectURL
+        textField.textField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    private lazy var 스테이지화면: TextField = {
+        let textField = TextField()
+        textField.title = "Stage"
+        textField.text = Environment.stage
         textField.textField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
         
         return textField
@@ -25,12 +52,15 @@ final class RootViewController: ViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        stackView.addArrangedSubview(테스트키화면)
+        stackView.addArrangedSubview(클라이언트키화면)
+        stackView.addArrangedSubview(커스터머키화면)
+        stackView.addArrangedSubview(브랜드키리다이렉트화면)
+        stackView.addArrangedSubview(스테이지화면)
         stackView.addArrangedSubview(테스트키리셋)
         stackView.addArrangedSubview(일반결제버튼)
         stackView.addArrangedSubview(위젯버튼)
         
-        테스트키리셋.title = "ClientKey 리셋"
+        테스트키리셋.title = "리셋"
         일반결제버튼.title = "일반결제 테스트"
         위젯버튼.title = "위젯 테스트"
         
@@ -50,13 +80,31 @@ final class RootViewController: ViewController {
     }
 
     @objc func textFieldDidChanged(_ sender: Any) {
-        if let clientKey = (sender as? UITextField)?.text {
-            Environment.clientKey = clientKey
+        guard let superview = (sender as? UITextField)?.superview else { return }
+        guard let text = (sender as? UITextField)?.text else { return }
+        switch superview {
+        case 클라이언트키화면:
+            Environment.clientKey = text
+        case 커스터머키화면:
+            Environment.customerKey = text
+        case 브랜드키리다이렉트화면:
+            Environment.brandPayRedirectURL = text
+        case 스테이지화면:
+            Environment.stage = text
+        default:
+            break
         }
     }
     
     @objc func resetClientKey() {
-        테스트키화면.text = Environment.Constant.defaultKey
-        Environment.clientKey = Environment.Constant.defaultKey
+        클라이언트키화면.text = Environment.Constant.defaultClientKey
+        Environment.clientKey = Environment.Constant.defaultClientKey
+        커스터머키화면.text = Environment.Constant.defaultCustomerKey
+        Environment.customerKey = Environment.Constant.defaultCustomerKey
+        브랜드키리다이렉트화면.text = Environment.Constant.defaultBrandpayRedirectURL
+        Environment.brandPayRedirectURL = Environment.Constant.defaultBrandpayRedirectURL
+        스테이지화면.text = Environment.Constant.defaultStage
+        Environment.stage = Environment.Constant.defaultStage
+        
     }
 }

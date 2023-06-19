a654dfdb640ae31a484a15f67d2acebf42634f4e
Marquez Kim <mqz@toss.im>
2023-06-19 20:33:59 +0900

    Merge pull request #27 from tosspayments/feat/testbox-key
    feat: 테스트 자동 배포 기능 추가
    # Conflicts:
    #	.github/workflows/ios.yml
    #	Examples/TossPayments-Examples/UIKitExample/UIKitExample/Sources/Environment/Environment.swift
    #	fastlane/Fastfile

--- Examples/TossPayments-Examples/UIKitExample/UIKitExample/Sources/Environment/Environment.swift
+++ Examples/TossPayments-Examples/UIKitExample/UIKitExample/Sources/Environment/Environment.swift
@@ -9,8 +9,6 @@ import Foundation
 
 public final class Environment {
     public enum Constant {
-        static let defaultKey = "test_ck_OyL0qZ4G1VODAxdNWDkroWb2MQYg"
-        
         public static let appScheme: String? = {
             guard let infoDictionary = Bundle.main.infoDictionary,
                   let bundleURLTypes = infoDictionary["CFBundleURLTypes"] as? [[String: Any]],
@@ -21,10 +19,25 @@ public final class Environment {
             }
             return scheme + "://"
         }()
+
+        static let defaultClientKey = "live_ck_JQbgMGZzorzj0ZnKbOjrl5E1em4d"
+        static let defaultCustomerKey = "TOSSPAYMENTS"
+        static let defaultBrandpayRedirectURL: String = {
+            if let infoDictionary = Bundle.main.infoDictionary,
+               let brandpayRedirectURL = infoDictionary["BRANDPAY_REDIRECT_URL"] as? String,
+               !brandPayRedirectURL.isEmpty {
+                return brandpayRedirectURL
+            }
+            return """
+            https://tosspayments.com/redierct
+            """
+        }()
+        static let defaultStage = "beta"
+        static let defaultVariantKey = "CardOnly"
     }
     
     static var clientKey: String = UserDefaults.standard.string(forKey: "TossPayments.ClientKey")
-    ?? Constant.defaultKey {
+    ?? Constant.defaultClientKey {
         didSet {
             UserDefaults.standard.set(clientKey, forKey: "TossPayments.ClientKey")
         }

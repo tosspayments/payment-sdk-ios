476c8bbdf71c8a44cc9aa7af9407c4654f7a147d
김진규 <mqz@toss.im>
2023-12-18 14:01:35 +0900

    feat: BaseTextField 추가

---
+++ Examples/UIKit-Objc-Example/Base/BaseTextField.h
@@ -0,0 +1,16 @@
+//
+//  BaseTextField.h
+//  UIKit-Objc-Example
+//
+//  Created by 김진규 on 2023/12/14.
+//  Copyright © 2023 TossPayments, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTextField : UIView
+
+@property UITextField *textField;
+
+@end
+

06ed0b70b03c74478b84fefd741f1f36647fcf4a
Jisoo <jisoo.youn@toss.im>
2024-05-06 15:52:24 +0900

    feat: 테스트홈에 알리페이 버튼 추가

---
+++ Examples/UIKit-Example/Sources/Alipay/AlipayViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AlipayViewController.swift
+//  UIKit-Example
+//
+//  Created by 윤지수 on 2024/05/06.
+//  Copyright © 2024 TossPayments, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+class AlipayViewController: UIViewController {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}

bf59eff26fd271e30ac6bf1466c2157859ff2fa3
Jisoo <jisoo.youn@toss.im>
2024-05-06 16:12:35 +0900

    feat: clientKey 입력받을 수 있도록 추가

--- Examples/UIKit-Example/Sources/Alipay/AlipayViewController.swift
+++ Examples/UIKit-Example/Sources/Alipay/AlipayViewController.swift
@@ -6,10 +6,36 @@
 //  Copyright © 2024 TossPayments, Inc. All rights reserved.
 //
 
-import SwiftUI
+#if canImport(UIKit)
+import UIKit
+import TossPayments
 
-class AlipayViewController: UIViewController {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class AlipayViewController: ViewController {
+    private lazy var 클라이언트키화면: TextField = {
+        let textField = TextField()
+        textField.title = "Client Key"
+        textField.text = Environment.clientKey
+        textField.textField.addTarget(self, action: #selector(textFieldDidChanged(_:)), for: .editingChanged)
+        
+        return textField
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stackView.addArrangedSubview(클라이언트키화면)
+    }
+    
+    @objc func textFieldDidChanged(_ sender: Any) {
+        guard let superview = (sender as? UITextField)?.superview else { return }
+        guard let text = (sender as? UITextField)?.text else { return }
+        switch superview {
+        case 클라이언트키화면:
+            Environment.clientKey = text
+        default:
+            break
+        }
     }
 }
+
+#endif

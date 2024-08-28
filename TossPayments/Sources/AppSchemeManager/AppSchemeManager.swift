289fc042b1625207ab6cd7a59e97765e12f5031c
JooYang <144883798+JooYang@users.noreply.github.com>
2024-08-28 22:08:25 +0900

    국민은행 리브next 앱 스킴 대응 (#68)
    * feat: 리브 next 앱 스킴 대응
    
    * feat: deprecate된 앱 스킴 제거

--- TossPayments/Sources/AppSchemeManager/AppSchemeManager.swift
+++ TossPayments/Sources/AppSchemeManager/AppSchemeManager.swift
@@ -58,8 +58,8 @@ enum AppSchemeManager: CaseIterable {
     case hyundaicardappcardid
     /// 페이나우
     case lguthepayxpay
-    /// Liiv 국민
-    case liivbank
+    /// 리브 next
+    case newliiv
     /// 토스
     case supertoss
     /// 우리WON뱅킹
@@ -118,8 +118,8 @@ enum AppSchemeManager: CaseIterable {
             return "com.wooricard.wcard"
         case .lguthepayxpay:
             return "lguthepay-xpay"
-        case .liivbank:
-            return "liivbank"
+        case .newliiv:
+            return "newliiv"
         case .supertoss:
             return "supertoss"
         case .newsmartpib:
@@ -144,6 +144,8 @@ enum AppSchemeManager: CaseIterable {
             return "id695436326"
         case .kbbank:
             return "id373742138"
+        case .newliiv:
+            return "id1573528126"
         case .samsung:
             return "id535125356"
         case .lottemobile:
@@ -180,8 +182,6 @@ enum AppSchemeManager: CaseIterable {
             return "id1499598869"
         case .lguthepayxpay:
             return "id760098906"
-        case .liivbank:
-            return "id1126232922"
         case .supertoss:
             return "id839333328"
         case .newsmartpib:
